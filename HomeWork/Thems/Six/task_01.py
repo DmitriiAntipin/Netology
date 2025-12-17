367cda2cc1ba05dc0bb1c8116407288865f21e3f
Aorus <antipindmitrij101@gmail.com>
2025-12-17 21:23:32 +0300

    Thems six

--- HomeWork/Thems/Six/task_01.py
+++ HomeWork/Thems/Six/task_01.py
@@ -1,5 +1,4 @@
 import json
-
 purchases = {}
 with open('purchase_log.json', 'r', encoding='utf-8') as f:
     for line in f:
@@ -8,21 +7,16 @@ with open('purchase_log.json', 'r', encoding='utf-8') as f:
         category = data.get('category')
         if user_id and category:
             purchases[user_id] = category
-
 with open('visit_log__1_.csv', 'r', encoding='utf-8') as visit_file, \
         open('funnel.csv', 'w', encoding='utf-8') as funnel_file:
     funnel_file.write('user_id,source,category\n')
-
     header = visit_file.readline()
-
     for line in visit_file:
         parts = line.strip().split(',')
         if len(parts) != 2:
             continue
         user_id, source = parts
-
         if user_id in purchases:
             category = purchases[user_id]
             funnel_file.write(f'{user_id},{source},{category}\n')
-
 print("Готово! Результат записан в funnel.csv")
